04eec156997a59a006c8275d098da77ae6d977b7
Ilyas Zainulin <tapo4el@gmail.com>
2022-08-04 15:05:19 +0200

    Task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://0dijv7m7d3.execute-api.us-east-1.amazonaws.com',
   bff: 'https://wyofw9a4rb.execute-api.us-east-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
